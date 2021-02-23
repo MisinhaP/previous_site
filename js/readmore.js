1b3e1a47ddfeb1e6deebe27f65f62ad139836067
David Martins <davidmartins@Davids-MacBook-Pro-2.local>
2021-02-23 23:15:29 +0100

    testimonials temporary fix

--- js/readmore.js
+++ js/readmore.js
@@ -1,12 +1,18 @@
 var myFunction = function() {
   var dots = document.getElementById("dots");
   var dots2 = document.getElementById("dots2");
+  var dots3 = document.getElementById("dots3");
+  var dots4 = document.getElementById("dots4");
 
   var moreText = document.getElementById("more");
   var moreText2 = document.getElementById("more2");
+  var moreText3 = document.getElementById("more3");
+  var moreText4 = document.getElementById("more4");
 
   var btnText = document.getElementById("readmore");
   var btnText2 = document.getElementById("readmore2");
+  var btnText3 = document.getElementById("readmore3");
+  var btnText4 = document.getElementById("readmore4");
 
 
   if (dots.style.display === "none") {
@@ -28,6 +34,27 @@ if (dots2.style.display === "none") {
     dots2.style.display = "none";
     btnText2.innerHTML = "Read less";
     moreText2.style.display = "inline";
+  }
+
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "Read more";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "Read less";
+    moreText3.style.display = "inline";
+  }
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "Read more";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "Read less";
+    moreText4.style.display = "inline";
   }
 
 
